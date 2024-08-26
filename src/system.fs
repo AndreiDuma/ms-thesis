16d1391161e8aca6d72d596fa3a79b23cb2470a1
Andrei Dorian Duma <andrei@andreiduma.ro>
2024-08-26 17:14:50 +0200

    Add some more (successful!) tests.

--- src/system.fs
+++ src/system.fs
@@ -1,12 +1,17 @@
-: ADIOS BYE ;  \ works
-\ ADIOS          \ works
-
-: PAPA ADIOS ;  \ works
-\ PAPA            \ works
-
 ( works )
 ( works )
 ( also ) ( works )
 \ line comment works.
 
 ( empty line ^ works )
+
+: ADIOS BYE ;  \ works
+\ ADIOS          \ works
+
+: PAPA ADIOS ;  \ works
+\ PAPA            \ works
+
+\ : DIE-IN-THE-MIDDLE [ BYE ] ;  \ works
+
+\ 1 2 3 4 dbg BYE  \ pushing to stack works
+
