c1ac9bae61741b8e73c98781f75c2c1562ed10fb
Andrei Dorian Duma <andrei@andreiduma.ro>
2024-08-29 21:59:24 +0200

    Major progress on the assembler; R-type done.

--- src/system.fs
+++ src/system.fs
@@ -1,62 +1,50 @@
 : DBG   dbg BYE ;
 : REG   reg BYE ;
 
+\ Navigate the stack.
 : ^ ( n -- )
   93 . 89 . 89 . 00 . ;                                     \ s3 += 8                                  addi s3, s3, 8
 : v ( n -- )											       
   93 . 89 . 89 . FF . ;                                     \ s3 -= 8                                  addi s3, s3, -8
-												       
+
+\ Load current stack item into register `t0` or `t1`.
 : >t ( n -- n )											       
   83 . B2 . 09 . 00 . ;	   			            \ t0 = [s3]                                ld t0, 0(s3)
-: t> ( ? -- n )											       
-  23 . B0 . 59 . 00 . ;		                            \ [s3] = t0                                sd t0, 0(s3)
-
 : >u ( n -- n )											       
   03 . B3 . 09 . 00 . ;	   			            \ t1 = [s3]                                ld t1, 0(s3)
+
+\ Store register `t0` or `t1` over current stack item.
+: t> ( ? -- n )											       
+  23 . B0 . 59 . 00 . ;		                            \ [s3] = t0                                sd t0, 0(s3)
 : u> ( ? -- n )											       
   23 . B0 . 69 . 00 . ;		                            \ [s3] = t1                                sd t1, 0(s3)
 
-: !>t ( n -- )  >t  ^ ;
-: t>! ( -- n )  v  t> ;
+\ "Float" current stack item "up" the stack, exchanging with item
+\ above.  Stack pointer follows the item.
+: % ( n1 n2 -- n2 .. )
+  >u ^  >t v
+  t> ^  u> ;
 
+\ Shorthand for compiling 4 bytes to `OUTPUT`, ensuring correct
+\ endianness.
 : :: ( ub1 ub2 ub3 ub4 -- )
-  [ ^ ^ ^ >t v v v t>! ] .                                  \ [s1++] = ub1  ( -- ub1 ub2 ub3 ub4 )
-  [ ^ ^   >t   v v t>! ] .                                  \ [s1++] = ub2  ( -- ub1 ub2 ub3 ub4 )
-  [ ^     >t     v t>! ] .                                  \ [s1++] = ub3  ( -- ub1 ub2 ub3 ub4 )
-  [                    ] .                                  \ [s1++] = ub4  ( -- ub1 ub2 ub3 ub4 )
+  [ ^ ^ ^ >t v v v v t> ] .                                 \ [s1++] = ub1  ( -- ub1 ub2 ub3 ub4 )
+  [ ^ ^   >t   v v v t> ] .                                 \ [s1++] = ub2  ( -- ub1 ub2 ub3 ub4 )
+  [ ^     >t     v v t> ] .                                 \ [s1++] = ub3  ( -- ub1 ub2 ub3 ub4 )
+  [                     ] .                                 \ [s1++] = ub4  ( -- ub1 ub2 ub3 ub4 )
   [ ^ ^ ^ ] ;		                                    \ s3 += 32      ( -- )  
 
+\ Compiles a 32-bit instruction (four bytes in the form of a 32-bit
+\ unsigned integer) to `OUTPUT`, ensuring correct endianness.
 : ` ( uw -- )
   [ 83 C2 09 00 :: ] 	   			            \ t0 = [s3 + 0]                            lbu t0, 0(s3)
-  [ t>! ] .                                                 \ [s3] = t0                                ---
+  [ v t> ] .                                                \ [s3] = t0                                ---
   [ 83 C2 19 00 :: ]				            \ t0 = [s3 + 1]                            lbu t0, 1(s3)
-  [ t>! ] .                                                 \ [s3] = t0                                ---
+  [ v t> ] .                                                \ [s3] = t0                                ---
   [ 83 C2 29 00 :: ]				            \ t0 = [s3 + 2]                            lbu t0, 2(s3)
-  [ t>! ] .                                                 \ [s3] = t0                                ---
+  [ v t> ] .                                                \ [s3] = t0                                ---
   [ 83 C2 39 00 :: ]			         	    \ t0 = [s3 + 3]                            lbu t0, 3(s3)
-  [ t>  ] . ;                                               \ [s3] = t0                                ---
-
-\ : t<< ( u -- )
-\   \ slli t0, t0, ?
-\   \ -> ~000000~​{{{shamt(??????)}}} {{{rs1(00101)}}} {{{fn3(001)}}} {{{rd(00101)}}} {{{op(0010011)}}}
-\   \ -> 0??29293
-\   [ >t ^ ]                                                  \ u@t0 = [s3++8]  ( -- )
-\   [ 93 92 42 01 :: ]                                        \ high@t0 = u@t0 << 20                     slli t0, t0, 20
-\   [ 37 33 29 29 :: ]					    \ low@t1 = 0x29293                         lui t1, 0x29293
-\   [ 13 53 C3 00 :: ]					    \                                          srli t1, t1, 12
-\   [ B3 E2 62 00 :: ]				            \ slli@t0 = high@t0 | low@t1               or t0, t0, t1
-\   [ v t> ] ` ;                                              \ [s1++4] = "slli ..."                     ---
-
-\ : t| ( u -- )
-\   \ ori t0, t0, 0x???
-\   \ -> {{{imm(0x???)}}} {{{rs1(00101)}}} {{{fn3(110)}}} {{{rd(00101)}}} {{{op(0010011)}}}
-\   \ -> ???2E293
-\   [ >t ^ ]                                                  \ u@t0 = [s3++8]  ( -- )                   ---
-\   [ 14 t<< ]                                                \ high@t0 = u@t0 << 20  ( -- )             ---
-\   [ 37 33 29 2E :: ]					    \ low@t1 = 0x2E293                         lui t1, 0x2E293
-\   [ 13 53 C3 00 :: ]					    \                                          srli t1, t1, 12
-\   [ B3 E2 62 00 :: ]					    \ ori@t0 = high@t0 | low@t1                or t0, t0, t1
-\   [ v t> ] ` ;						    \ [s1++4] = "ori ..."                      ---
+  [   t> ] . ;                                              \ [s3] = t0                                ---
 
 : SWAP ( n1 n2 -- n2 n1 )
   [ >u ^ >t ]
@@ -104,30 +92,41 @@
   1 SWAP << 1-		        \ mask = (1 << len) - 1    ( -- m mask )
   & ;                           \ n' = m & mask            ( -- n' )
 
+: % ( n1 n2 -- n2 .. )  SWAP [ ^ ] ;
+
+\ R-type instructions.
+: rinstr ( rd rs1 rs2 fn7 fn3 op -- instr )
+  % % % % % [ v v v v v ]  ( -- op rd rs1 rs2 fn7 fn3 )
+  % % %     [     v v v ]  ( -- op rd fn3 rs1 rs2 fn7 )
+  5 << | 5 << | 3 << | 5 << | 7 << | ;
+: `add  ( rd rs1 rs2 -- instr )  00 0 33 rinstr ` ;
+: `sub  ( rd rs1 rs2 -- instr )  20 0 33 rinstr ` ;
+: `sll  ( rd rs1 rs2 -- instr )  00 1 33 rinstr ` ;
+: `slt  ( rd rs1 rs2 -- instr )  00 2 33 rinstr ` ;
+: `sltu ( rd rs1 rs2 -- instr )  00 3 33 rinstr ` ;
+: `xor  ( rd rs1 rs2 -- instr )  00 4 33 rinstr ` ;
+: `srl  ( rd rs1 rs2 -- instr )  00 5 33 rinstr ` ;
+: `sra  ( rd rs1 rs2 -- instr )  20 5 33 rinstr ` ;
+: `or   ( rd rs1 rs2 -- instr )  00 6 33 rinstr ` ;
+: `and  ( rd rs1 rs2 -- instr )  00 7 33 rinstr ` ;
+\ RV64 instructions
+: `addw ( rd rs1 rs2 -- instr )  00 0 3B rinstr ` ;
+: `subw ( rd rs1 rs2 -- instr )  20 0 3B rinstr ` ;
+: `sllw ( rd rs1 rs2 -- instr )  00 1 3B rinstr ` ;
+: `srlw ( rd rs1 rs2 -- instr )  00 5 3B rinstr ` ;
+: `sraw ( rd rs1 rs2 -- instr )  20 5 3B rinstr ` ;
 
 
+: +' ( n1 n2 -- n )
+  [ >u ^ >t ]
+  [ 05 05 06 `add ]
+  [ t> ] ;
 
-
-
-
-
-: x: ( ul ur -- )
-  [ 03 . B3 . 09 . 00 . ] ^				    \ ur@t1 = [s3++8]                       ld t1, 0(s3)
-  [ 83 . B2 . 09 . 00 . ] ^				    \ ul@t0 = [s3++8]                       ld t0, 0(s3)
-  [ 33 . 5A . 6A . 00 . ]				    \ x@s4 >>= ur@t1                        srl s4, s4, t1
-  [ B3 . 82 . 62 . 40 . ]				    \ mask@t1 = (0xFF << (ul - ur)) >> 7    sub t0, t0, t1
-  [ 13 . 03 . F0 . 0F . ]				    \                                       addi t1, zero, 0xFF
-  [ 33 . 13 . 53 . 00 . ]				    \                                       sll t1, t1, t0
-  [ 13 . 53 . 73 . 00 . ]				    \                                       srli t1, t1, 7
-  [ 33 . 7A . 6A . 00 . ] ;				    \ x@s4 &= mask@t1                       and s4, s4, t1
+3 4 +' 5 +'
+DBG
 
   
 
-
-: + ( n1 n2 -- n )
-  [ !>t1 !>t0 ]
-  [ t0 t0 t1 add ]
-  [ t0>! ] ;
 
 : add ( rd rs1 rs2 -- )
   [ 0000000 000 0110011 rinstr ] ;
@@ -138,130 +137,6 @@
   [ ... .. binstr ] ;
 
 
-: .>x ( n -- n )
-  03 . BA . 09 . 00 . ;				            \ x@s4 = [s3]                           ld s4, 0(s3)
-: x>. ( ? -- n )
-  23 . B0 . 49 . 01 . ;	       			            \ [s3] = x@s4                           sd s4, 0(s3)
-
-\ : .>y ( n -- n )
-\   83 . BA . 09 . 00 . ;				            \ x@s5 = [s3]                           ld s5, 0(s3)
-\ : y>. ( ? -- n )
-\   23 . B0 . 59 . 01 . ;	       			            \ [s3] = x@s5                           sd s5, 0(s3)
-  
-: x<< ( u -- )
-  \ slli s4, s4, ?
-  \ -> ~000000~​{{{shamt(??.????)}}} {{{rs1(1010.0)}}} {{{fn3(001)}}} {{{rd(1010.0)}}} {{{op(0010011)}}}
-  \ -> 13 1A ?A 0?
-  13 . 1A .	                                            \ [s1++] = ...
-
-  [ .>t0 ]
-  [ 93 . F2 . F2 . 00 . ]                                   \ t0 &= 0b00001111                      andi t0, t0, 0x0F
-  [ 93 . 92 . 42 . 00 . ]                                   \ t0 <<= 4)                             slli t0, t0, 4
-  [ 93 . E2 . A2 . 00 . ]                                   \ t0 |= 0b00001010                      ori t0, t0, 0x0A
-  [ v t0>. ] .
-
-  [ .>t0 ]
-  [ 93 . D2 . 22 . 00 . ]				    \ t0 >>= 2                              srli t0, t0, 2
-  [ t0>. ] . ;
-
-: x| ( u -- )
-  \ ori s4, s4, ?
-  \ -> {{{imm(0x???)}}} {{{rs1(10100)}}} {{{fn3(110)}}} {{{rd(10100)}}} {{{op(0010011)}}}
-  \ -> 13 6A ?A ??    |     ???A6A13
-  [ .>t0 ^ ]                                                \ t0 = u
-  [ A6A13 .>t1 ^ ]					    \ t1 = A6A13
-  [ 01429293 $ ]	                                    \ t0 <<= 20                             slli t0, t0, 20
-  [ 0062E2B3 $ ] $ ;					    \ t0 |= t1                              or t0, t0, t1
-
-
-: 2* ( n -- n' )
-  [ .>x  1 x<<  x>. ] ;
-
-: 1+ ( n -- n' )
-  [ .>x ]
-  [ 13 . 0A . 1A . 00 . ]	                            \ x@s4 += 1                             addi s4, s4, 1
-  [ x>. ] ;
-
-
-: x: ( ul ur -- )
-  [ 03 . B3 . 09 . 00 . ] ^				    \ ur@t1 = [s3++8]                       ld t1, 0(s3)
-  [ 83 . B2 . 09 . 00 . ] ^				    \ ul@t0 = [s3++8]                       ld t0, 0(s3)
-  [ 33 . 5A . 6A . 00 . ]				    \ x@s4 >>= ur@t1                        srl s4, s4, t1
-  [ B3 . 82 . 62 . 40 . ]				    \ mask@t1 = (0xFF << (ul - ur)) >> 7    sub t0, t0, t1
-  [ 13 . 03 . F0 . 0F . ]				    \                                       addi t1, zero, 0xFF
-  [ 33 . 13 . 53 . 00 . ]				    \                                       sll t1, t1, t0
-  [ 13 . 53 . 73 . 00 . ]				    \                                       srli t1, t1, 7
-  [ 33 . 7A . 6A . 00 . ] ;				    \ x@s4 &= mask@t1                       and s4, s4, t1
-
-: x<< ( u -- )
-  [ 83 . B2 . 09 . 00 . ] ^				    \ u@t0 = [s3++8]                        ld t0, 0(s3)
-  [ 33 . 1A . 5A . 00 . ] ;				    \ x@s4 <<= u@t0                         sll s4, s4, t0
-
-: x| ( u -- )
-  [ 83 . B2 . 09 . 00 . ] ^				    \ u@t0 = [s3++8]                        ld t0, 0(s3)
-  [ 33 . 6A . 5A . 00 . ] ;				    \ x@s4 |= u@t0                          or s4, s4, t0
-
-: >x ( n -- n )
-  \ ld s4, n(s3)
-  \ {{{off(0x??.?)}}} {{{rs1(1001.1)}}} {{{fn3(011)}}} {{{rd(1010.0)}}} {{{op(0000011)}}}
-  \ 03 BA ?9 ??
-  03 . BA .				                    \ [s1++] = 0x03; [s1++] = 0xBA
-  .>x  3 0 x:  4 x<<  09 x|  0 x>. .
-  .>x  B 4 x:                  x>. . ;
-
-: x> ( ? -- n )
-  \ ld s4, n(s3)
-  \ {{{off(???????)}}} {{{rs2(1.0100)}}} {{{rs1(1001.1)}}} {{{fn3(011)}}} {{{off(????.?)}}} {{{op(0100011)}}}
-  \ ?3 B? 49 ??
-  .>x  0 0 x:  7 x<<  23 x|  0 x>. .
-  .>x  4 1 x:         B0 x|  0 x>. .
-  49                               .
-  .>x  B 5 x:  1 x<<  01 x|    x>. . ;
-
-: NEGATE ( n -- n' )
-  [ 83 . B2 . 09 . 00 . ] 				    \ n@t0 = [s3]                           ld t0, 0(s3)
-  [ B3 . 02 . 50 . 40 . ]                                   \ n@t0 = -n@t0                          sub t0, zero, t0
-  [ 23 . B0 . 59 . 00 . ] ;				    \ [s3] = n@t0                           sd t0, 0(s3)
-
-: test [ 8 >x ]
-       [ 0 x> ]
-       [ 8 x> ]
-       [ 8 NEGATE x> ] ;
-
-1 2 3 test
-dbg BYE
-
-
-
-
-
-
-: 2* ( n -- n' )
-  .>x
-  1 x<<
-  x>. ;
-
-
-
-
-
-
-
-\ : + ( n1 n2 -- n )
-\   [ t0 0 s3 ld ]
-\   [ t1 8 s3 ld ]
-\   [ t0 t0 t1 add ]
-\   [ s3 s3 8 addi ]
-\   [ t0 8 s3 sd ] ;
-
-: ROT ( n1 n2 n3 -- n2 n3 n1 )
-  10 >x 8 >y 0 >x
-  10 z> 8 y> 0 x> ;
-
-: + ( n1 n2 -- n )
-  0 >x ^ 0 >y
-  [ add ... ]
-  0 x> ;
 
 
 
