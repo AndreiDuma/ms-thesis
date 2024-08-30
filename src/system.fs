b1965b7f0ce8dde8b21f62654230800e35760e54
Andrei Dorian Duma <andrei@andreiduma.ro>
2024-08-30 23:16:20 +0200

    Implemented CONSTANT.

--- src/system.fs
+++ src/system.fs
@@ -25,15 +25,15 @@
   >t1 ^  >t0 v
   t0> ^  t1> ;
 
-: DROP ( n -- )  [ ^ ] ;
-: DUP  ( n -- n n )  [ >t0 v t0> ] ;
-: SWAP ( n1 n2 -- n2 n1 )  [ % v ] ;
-: OVER ( n1 n2 -- n1 n2 n1 )  [ ^ >t0 v v t0> ] ;
-: ROT  ( n1 n2 n3 -- n2 n3 n1 )  [ ^ % v v % v ]  ;
-: 2SWAP ( d1 d2 -- d2 d1 )  [ ^ % % v v v % % v v ] ;
-: 2DUP ( d -- d d )  [ ^ >t0 v >t1 v t0> v t1> ] ;
-: 2OVER ( d1 d2 -- d1 d2 d1 )  [ ^ ^ ^ >t0 v >t1 v v v t0> v t1> ] ;
-: 2DROP ( d1 d2 -- d1 )  [ ^ ^ ] ;
+: DROP ( n -- )                   [ ^ ] ;
+: DUP  ( n -- n n )               [ >t0 v t0> ] ;
+: SWAP ( n1 n2 -- n2 n1 )         [ % v ] ;
+: OVER ( n1 n2 -- n1 n2 n1 )      [ ^ >t0 v v t0> ] ;
+: ROT  ( n1 n2 n3 -- n2 n3 n1 )   [ ^ % v v % v ]  ;
+: 2SWAP ( d1 d2 -- d2 d1 )        [ ^ % % v v v % % v v ] ;
+: 2DUP ( d -- d d )               [ ^ >t0 v >t1 v t0> v t1> ] ;
+: 2OVER ( d1 d2 -- d1 d2 d1 )     [ ^ ^ ^ >t0 v >t1 v v v t0> v t1> ] ;
+: 2DROP ( d1 d2 -- d1 )           [ ^ ^ ] ;
 
 
 \ --- RISC-V Assembler --- \
@@ -86,22 +86,22 @@
   [ % % % % % v v v v v ]  ( -- op rd rs1 rs2 fn7 fn3 )
   [ % % %         v v v ]  ( -- op rd fn3 rs1 rs2 fn7 )
   `instr/rsb ;
-: `add  ( rd rs1 rs2 -- )  00 0 33 `instr/r ;
-: `sub  ( rd rs1 rs2 -- )  20 0 33 `instr/r ;
-: `sll  ( rd rs1 rs2 -- )  00 1 33 `instr/r ;
-: `slt  ( rd rs1 rs2 -- )  00 2 33 `instr/r ;
-: `sltu ( rd rs1 rs2 -- )  00 3 33 `instr/r ;
-: `xor  ( rd rs1 rs2 -- )  00 4 33 `instr/r ;
-: `srl  ( rd rs1 rs2 -- )  00 5 33 `instr/r ;
-: `sra  ( rd rs1 rs2 -- )  20 5 33 `instr/r ;
-: `or   ( rd rs1 rs2 -- )  00 6 33 `instr/r ;
-: `and  ( rd rs1 rs2 -- )  00 7 33 `instr/r ;
-\ RV64 instructions.
-: `addw ( rd rs1 rs2 -- )  00 0 3B `instr/r ;
-: `subw ( rd rs1 rs2 -- )  20 0 3B `instr/r ;
-: `sllw ( rd rs1 rs2 -- )  00 1 3B `instr/r ;
-: `srlw ( rd rs1 rs2 -- )  00 5 3B `instr/r ;
-: `sraw ( rd rs1 rs2 -- )  20 5 3B `instr/r ;
+: `add  ( rd rs1 rs2 -- )   00 0 33 `instr/r ;
+: `sub  ( rd rs1 rs2 -- )   20 0 33 `instr/r ;
+: `sll  ( rd rs1 rs2 -- )   00 1 33 `instr/r ;
+: `slt  ( rd rs1 rs2 -- )   00 2 33 `instr/r ;
+: `sltu ( rd rs1 rs2 -- )   00 3 33 `instr/r ;
+: `xor  ( rd rs1 rs2 -- )   00 4 33 `instr/r ;
+: `srl  ( rd rs1 rs2 -- )   00 5 33 `instr/r ;
+: `sra  ( rd rs1 rs2 -- )   20 5 33 `instr/r ;
+: `or   ( rd rs1 rs2 -- )   00 6 33 `instr/r ;
+: `and  ( rd rs1 rs2 -- )   00 7 33 `instr/r ;
+\ RV64 instructions.	   
+: `addw ( rd rs1 rs2 -- )   00 0 3B `instr/r ;
+: `subw ( rd rs1 rs2 -- )   20 0 3B `instr/r ;
+: `sllw ( rd rs1 rs2 -- )   00 1 3B `instr/r ;
+: `srlw ( rd rs1 rs2 -- )   00 5 3B `instr/r ;
+: `sraw ( rd rs1 rs2 -- )   20 5 3B `instr/r ;
 
 \ I-type instructions.
 : `instr/i ( rd rs1 imm fn3 op -- )
@@ -113,29 +113,29 @@
   5 << |	 ( -- rd rs1 fn3 op imm )
   ROT ROT	 ( -- rd rs1 imm fn3 op )
   `instr/i ;
-: `ecall ( -- )               0 0 000 0 73 `instr/i ;
-: `jalr  ( rd rs1 imm   -- )          0 67 `instr/i ;
-: `lb    ( rd rs1 imm   -- )          0 03 `instr/i ;
-: `lh    ( rd rs1 imm   -- )          1 03 `instr/i ;
-: `lw    ( rd rs1 imm   -- )          2 03 `instr/i ;
-: `lbu   ( rd rs1 imm   -- )          4 03 `instr/i ;
-: `lhu   ( rd rs1 imm   -- )          5 03 `instr/i ;
-: `addi  ( rd rs1 imm   -- )          0 13 `instr/i ;
-: `slti  ( rd rs1 imm   -- )          2 13 `instr/i ;
-: `sltiu ( rd rs1 imm   -- )          3 13 `instr/i ;
-: `xori  ( rd rs1 imm   -- )          4 13 `instr/i ;
-: `ori   ( rd rs1 imm   -- )          6 13 `instr/i ;
-: `andi  ( rd rs1 imm   -- )          7 13 `instr/i ;
-: `slli  ( rd rs1 shamt -- )       00 1 13 `instr/i/shift ;
-: `srli  ( rd rs1 shamt -- )       00 5 13 `instr/i/shift ;
-: `srai  ( rd rs1 shamt -- )       20 5 13 `instr/i/shift ;
-\ RV64 instructions.	          
-: `lwu   ( rd rs1 imm   -- )          6 03 `instr/i ;
-: `ld    ( rd rs1 imm   -- )          3 03 `instr/i ;
-: `addiw ( rd rs1 imm   -- )          0 1B `instr/i ;
-: `slliw ( rd rs1 shamt -- )       00 1 1B `instr/i/shift ;
-: `srliw ( rd rs1 shamt -- )       00 5 1B `instr/i/shift ;
-: `sraiw ( rd rs1 shamt -- )       20 5 1B `instr/i/shift ;
+: `ecall ( -- )                0 0 000 0 73 `instr/i ;
+: `jalr  ( rd rs1 imm   -- )           0 67 `instr/i ;
+: `lb    ( rd rs1 imm   -- )           0 03 `instr/i ;
+: `lh    ( rd rs1 imm   -- )           1 03 `instr/i ;
+: `lw    ( rd rs1 imm   -- )           2 03 `instr/i ;
+: `lbu   ( rd rs1 imm   -- )           4 03 `instr/i ;
+: `lhu   ( rd rs1 imm   -- )           5 03 `instr/i ;
+: `addi  ( rd rs1 imm   -- )           0 13 `instr/i ;
+: `slti  ( rd rs1 imm   -- )           2 13 `instr/i ;
+: `sltiu ( rd rs1 imm   -- )           3 13 `instr/i ;
+: `xori  ( rd rs1 imm   -- )           4 13 `instr/i ;
+: `ori   ( rd rs1 imm   -- )           6 13 `instr/i ;
+: `andi  ( rd rs1 imm   -- )           7 13 `instr/i ;
+: `slli  ( rd rs1 shamt -- )        00 1 13 `instr/i/shift ;
+: `srli  ( rd rs1 shamt -- )        00 5 13 `instr/i/shift ;
+: `srai  ( rd rs1 shamt -- )        20 5 13 `instr/i/shift ;
+\ RV64 instructions.	           
+: `lwu   ( rd rs1 imm   -- )           6 03 `instr/i ;
+: `ld    ( rd rs1 imm   -- )           3 03 `instr/i ;
+: `addiw ( rd rs1 imm   -- )           0 1B `instr/i ;
+: `slliw ( rd rs1 shamt -- )        00 1 1B `instr/i/shift ;
+: `srliw ( rd rs1 shamt -- )        00 5 1B `instr/i/shift ;
+: `sraiw ( rd rs1 shamt -- )        20 5 1B `instr/i/shift ;
 
 \ S-type instructions.
 : `instr/s ( rs2 rs1 offset fn3 op -- )
@@ -146,10 +146,10 @@
   B 5 [:]              ( -- op imm5 fn3 rs2 rs1 imm7 )
   [ ^ %         v v ]  ( -- op imm5 fn3 rs1 rs2 imm7 )
   `instr/rsb ;
-: `sb  ( rs2 rs1 offset -- )  0 23 `instr/s ;
-: `sh  ( rs2 rs1 offset -- )  1 23 `instr/s ;
-: `sw  ( rs2 rs1 offset -- )  2 23 `instr/s ;
-: `sd  ( rs2 rs1 offset -- )  3 23 `instr/s ;
+: `sb  ( rs2 rs1 offset -- )   0 23 `instr/s ;
+: `sh  ( rs2 rs1 offset -- )   1 23 `instr/s ;
+: `sw  ( rs2 rs1 offset -- )   2 23 `instr/s ;
+: `sd  ( rs2 rs1 offset -- )   3 23 `instr/s ;
 
 \ B-type instructions.
 : `instr/b ( rs1 rs2 offset fn3 op -- )
@@ -161,12 +161,12 @@
   DUP  C C [:]	        ( -- op imm5 fn3 rs1 rs2 offset offset[12] )
   6 << SWAP  A 5 [:] |  ( -- op imm5 fn3 rs1 rs2 imm7 )
   `instr/rsb ;
-: `beq  ( rs1 rs2 offset -- )  0 63 `instr/b ;
-: `bne  ( rs1 rs2 offset -- )  1 63 `instr/b ;
-: `blt  ( rs1 rs2 offset -- )  4 63 `instr/b ;
-: `bge  ( rs1 rs2 offset -- )  5 63 `instr/b ;
-: `bltu ( rs1 rs2 offset -- )  6 63 `instr/b ;
-: `bgeu ( rs1 rs2 offset -- )  7 63 `instr/b ;
+: `beq  ( rs1 rs2 offset -- )   0 63 `instr/b ;
+: `bne  ( rs1 rs2 offset -- )   1 63 `instr/b ;
+: `blt  ( rs1 rs2 offset -- )   4 63 `instr/b ;
+: `bge  ( rs1 rs2 offset -- )   5 63 `instr/b ;
+: `bltu ( rs1 rs2 offset -- )   6 63 `instr/b ;
+: `bgeu ( rs1 rs2 offset -- )   7 63 `instr/b ;
 
 \ Common format for U/J-type instructions.
 : `instr/uj  ( op rd imm20 -- )
@@ -175,8 +175,8 @@
 \ U-type instructions.
 : `instr/u ( rd imm opcode -- )
   ROT ROT `instr/uj ;
-: `lui   ( rd imm -- )  37 `instr/u ;
-: `auipc ( rd imm -- )  17 `instr/u ;
+: `lui   ( rd imm -- )   37 `instr/u ;
+: `auipc ( rd imm -- )   17 `instr/u ;
 
 \ J-type instructions.
 : `instr/j ( rd offset opcode -- )
@@ -203,8 +203,8 @@
 \ --- Common FORTH words --- \
 
 \ Arithmetic.
-: NEGATE ( n -- n' )     [ >t0 ]       [ t0 zero t0 `sub ] [ t0> ] ;
-: +      ( n1 n2 -- n )  [ >t1 ^ >t0 ] [ t0   t0 t1 `add ] [ t0> ] ;
+: NEGATE ( n -- n' )      [ >t0 ]       [ t0 zero t0 `sub ] [ t0> ] ;
+: +      ( n1 n2 -- n )   [ >t1 ^ >t0 ] [ t0   t0 t1 `add ] [ t0> ] ;
 : *      ( n1 n2 -- n )           ;
 : /MOD   ( n1 n2 -- rem quot )    ;
 : /      ( n1 n2 -- quot )        ;
@@ -219,30 +219,31 @@
 : ABS    ( n -- n' )     [ >t0   >t1 ] [ t1 t1  3F `srai ]            \ mask@t1 >>= 63
 	                               [ t0 t0  t1 `xor  ]            \ t0 = t0 xor mask@t1
 				       [ t0 t0  t1 `sub  ] [ t0> ] ;  \ t0 -= mask@t1
-: MIN    ( n1 n2 -- nmin )  2DUP -          ( -- n1 n2 n1-n2 )
-                            DUP ABS         ( -- n1 n2 n1-n2 |n1-n2| )
-			    - 2/ +          ( -- n1 nmin )
-			    SWAP DROP ;     ( -- nmin )
-: MAX    ( n1 n2 -- n )  2DUP -  DUP ABS  + 2/ +  SWAP DROP ;
+: MIN    ( n1 n2 -- nmin )   2DUP -          ( -- n1 n2 n1-n2 )
+                             DUP ABS         ( -- n1 n2 n1-n2 |n1-n2| )
+			     - 2/ +          ( -- n1 nmin )
+			     SWAP DROP ;     ( -- nmin )
+: MAX    ( n1 n2 -- n )   2DUP -  DUP ABS  + 2/ +  SWAP DROP ;
 
 \ Memory access.
-: C! ( c addr -- )  [ >t1 ^ >t0 ^ ] [ t0 t1  0 `sb  ]         ;
-: C@ ( addr -- c )  [ >t1         ] [ t0 t1  0 `lbu ] [ t0> ] ;
-: !  ( n addr -- )  [ >t1 ^ >t0 ^ ] [ t0 t1  0 `sd  ]         ;
-: @  ( addr -- n )  [ >t1         ] [ t0 t1  0 `ld  ] [ t0> ] ;
-: +! ( n addr -- )  DUP @  ( -- n addr n0 )
-                    ROT +  ( -- addr n' )
-		    SWAP ! ;
+: C! ( c addr -- )   [ >t1 ^ >t0 ^ ] [ t0 t1  0 `sb  ]         ;
+: C@ ( addr -- c )   [ >t1         ] [ t0 t1  0 `lbu ] [ t0> ] ;
+: !  ( n addr -- )   [ >t1 ^ >t0 ^ ] [ t0 t1  0 `sd  ]         ;
+: @  ( addr -- n )   [ >t1         ] [ t0 t1  0 `ld  ] [ t0> ] ;
+: +! ( n addr -- )   DUP @  ( -- n addr n0 )
+                     ROT +  ( -- addr n' )
+		     SWAP ! ;
 
 \ Dictionary.
-: ALLOT ( n -- )  [ >t0 ^       ] [ s1 s1 t0 `add ] ;
-: CHARS  ( n -- n' )       ;
-: INSTRS ( n -- n' )  2 << ;
-: CELLS  ( n -- n' )  3 << ;
+: ALLOT ( n -- )   [ >t0 ^ ] [ s1 s1 t0 `add ] ;
+: CHARS  ( n -- n' )        ;
+: INSTRS ( n -- n' )   2 << ;
+: CELLS  ( n -- n' )   3 << ;
+
 : , ( n -- )
-  [ >t0 ^ ] [ t0 s1 0 `sd ]
-  1 CELLS ALLOT ;
-: C, ( n -- )  . ;
+  1 CELLS ALLOT
+  [ >t0 ^ ] [ t0 s1  8 NEGATE  `sd ] ;
+: C, ( n -- )   . ;
 
 : LITERAL ( C: n -- ) ( -- n )
   \ Compile "lui t0, 0xHHHHH[+1]".
@@ -254,57 +255,52 @@
   \ Compile a sequence that pushes `t0` on the stack.
   v t0> ;
 
-: CREATE ( "ccc<SPC>" -- )
-  pname \ [ v ] [ a0 s3 0 `sd ]
-        \ [ v ] [ a1 s3 0 `sd ]
+: create ( C: "ccc<SPC>" -- )
+  pname
   [ v ] [ s0 s3  0 `sd   ]	   \ save INPUT@s0
         [ s0 a0  0 `addi ]	   \ INPUT@s0 = addr@a0
 	[ a0 s2 28 `ld   ]	   \ latest@a0 = [LATEST] (as required by Head)
-  Head  [ s0 s3  0 `ld   ] [ ^ ]   \ restore INPUT@s0
+  Head  [ s0 s3  0 `ld   ] [ ^ ] ; \ restore INPUT@s0
+  
+: CREATE ( C: "ccc<SPC>" -- ) ( -- addr )
+  create
   \ Compile code that pushes on the stack the address of the empty
   \ space following the CREATEd definition.
   t0 0 `auipc
   t0 t0 14 `addi
   v t0>
-\  \ Compile code that pushes current OUTPUT@s2 on the stack.
-\  [ v ] [ s1 s3 0 `sd ] LITERAL
   \ Compile "jalr zero, 0(ra)".
   zero ra 0 `jalr ;
 
-: VARIABLE ( -- )   CREATE  0 , ;
+: VARIABLE ( C: "ccc<SPC>" -- ) ( -- addr )   CREATE  0 , ;
+: CONSTANT ( C: "ccc<SPC>" n -- ) ( -- n )    create  LITERAL  zero ra 0 `jalr ;
 
 
+\ : IMMEDIATE ( -- ) ;
+\ : ' ( "ccc<SPC>" -- addr ) ;			       
 
-\ : CONSTANT ( "ccc<SPC>" n -- ) ( -- n )
-\   CREATE
-  
-\ DBG
+\ \ Return stack management.
+\ : >>t0 ( R: n -- n )  [ t0 sp 0 `ld ] ;
+\ : t0>> ( R: ? -- n )  [ t0 sp 0 `sd ] ;
+\ : ^^   ( R: n -- .. )  [ sp sp   8 `addi ] ;
+\ : vv   ( R: .. -- n )  [ sp sp FF8 `addi ] ;
+\ : R@    ( -- n ) ( R: n -- n )  [ >>t0    ] [  v t0>  ] ;
 
+\ : >R    ( n -- ) ( R: -- n )    [  >t0  ^ ] [ vv t0>> ] ;
+\ : R>    ( -- n ) ( R: n -- )    [ >>t0 ^^ ] [  v t0>  ] ;
 
-
-
-: IMMEDIATE ( -- ) ;
-: ' ( "ccc<SPC>" -- addr ) ;
-			       
-: VARIABLE ( C: "ccc" -- ) ( -- addr ) ;
-: CONSTANT ( C: "ccc" n -- ) ( -- n ) ;
-
-\ Return stack management.
-: >>t0 ( R: n -- n )  [ t0 sp 0 `ld ] ;
-: t0>> ( R: ? -- n )  [ t0 sp 0 `sd ] ;
-: ^^   ( R: n -- .. )  [ sp sp   8 `addi ] ;
-: vv   ( R: .. -- n )  [ sp sp FF8 `addi ] ;
-: >R    ( n -- ) ( R: -- n )    [  >t0  ^ ] [ vv t0>> ] ;
-: R>    ( -- n ) ( R: n -- )    [ >>t0 ^^ ] [  v t0>  ] ;
-: R@    ( -- n ) ( R: n -- n )  [ >>t0    ] [  v t0>  ] ;
-
-R> DBG
 \ PROBLEM: Colon installs prologues and epilogues that mess with the
 \ return stack.  We need a way to create "bare" definitions with no
 \ epilogue or prologue.
 
 \ Control flow.
 : IF ( flag -- ) ;
+
+
+
+
+
+
 : = ( n1 n2 -- flag ) ;
 : <> ( n1 n2 -- flag ) ;
 : < ( n1 n2 -- flag ) ;
